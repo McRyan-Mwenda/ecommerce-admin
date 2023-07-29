9aae92a530935cc0f4d90818b629b7d3e097bfed
McRyan-Mwenda <61878039+McRyan-Mwenda@users.noreply.github.com>
2023-07-29 16:37:52 +0300

    Billboards entity

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -24,6 +24,7 @@ import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
   label: z.string().min(1),
@@ -62,9 +63,14 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
   const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true);
-      await axios.patch(`/api/stores/${params.storeId}`, data);
+      if (initialData) {
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      }
       router.refresh();
-      toast.success("Store updated.");
+      router.push(`/${params.storeId}/billboards`)
+      toast.success(toastMessage);
     } catch (error) {
       toast.error("Something went wrong.");
     } finally {
@@ -75,12 +81,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
   const OnDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/stores/${params.storeId}`);
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
       router.refresh();
       router.push("/");
-      toast.success("Store deleted successfully");
+      toast.success("Billboard deleted successfully");
     } catch (error) {
-      toast.error("Make sure you removed all products and categories first.");
+      toast.error("Make sure you removed all categories using this billboard first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -99,15 +105,14 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
         <Heading title={title} description={description} />
         {initialData && (
           <Button
-          variant="destructive"
-          size="icon"
-          onClick={() => setOpen(true)}
-          disabled={loading}
-        >
-          <Trash className="w-4 h-4" />
-        </Button>
+            variant="destructive"
+            size="icon"
+            onClick={() => setOpen(true)}
+            disabled={loading}
+          >
+            <Trash className="w-4 h-4" />
+          </Button>
         )}
-        
       </div>
 
       <Separator />
@@ -117,6 +122,24 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background Image</FormLabel>
+                <FormControl>
+                  <ImageUpload 
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
@@ -142,7 +165,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
         </form>
       </Form>
       <Separator />
-      
     </>
   );
 };
--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Trash } from "lucide-react";
+import { ImagePlus, Trash } from "lucide-react";
 import Image from "next/image";
 import { CldUploadWidget } from "next-cloudinary";
 
@@ -35,20 +35,20 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
 
   return (
     <div>
-      <div className="mb-4 flex items-center gap-4">
+      <div className="flex gap-4 items-center mb-4">
         {value.map((url) => (
           <div
             key={url}
-            className="reative w-[200px] h-[200px] rounded-md overflow-hidden"
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
           >
-            <div className="z-10 absolute top-2 right-2">
+            <div className="absolute top-2 right-2 z-10">
               <Button
                 type="button"
                 onClick={() => onRemove(url)}
                 variant="destructive"
                 size="icon"
               >
-                <Trash className="h-4 w-4" />
+                <Trash className="w-4 h-4" />
               </Button>
             </div>
             <Image fill className="object-cover" alt="image" src={url} />
@@ -58,7 +58,25 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
       <CldUploadWidget
         onUpload={onUpload}
         uploadPreset="yeoqye74"
-      ></CldUploadWidget>
+      >
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          }
+
+          return (
+            <Button 
+              type="button" 
+              disabled={disabled} 
+              variant="secondary"
+              onClick={onClick}
+            >
+              <ImagePlus className="mr-2 w-4 h-4"/>
+              Upload an image
+            </Button>
+          )
+        }}
+      </CldUploadWidget>
     </div>
   );
 };
